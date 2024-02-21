76d1a9c12d829a55b0bc06f30e7bf42ac9fea4a3
ThibaultCuisiniere <thibault.cuisiniere@umontreal.ca>
2024-02-21 12:13:57 -0500

    Add sig_label option to allow adding stars reflecting pval in the legend

--- R/Plot_microbiota.R
+++ R/Plot_microbiota.R
@@ -6,49 +6,49 @@
 #' @param exp_group a column in sample_data containing the experimental group information
 #' @param subset_group default NULL. Groups among the 'exp_group' column to subset
 #' @param sample_name name of the column in sample_data containing the unique sample identifier
-#' @param main_level  Default 'Phylum'. Level to which features will be clustered 
-#' @param sub_level Default 'Family'. Level to which taxa will be plotted and analyzed 
+#' @param main_level  Default 'Phylum'. Level to which features will be clustered
+#' @param sub_level Default 'Family'. Level to which taxa will be plotted and analyzed
 #' @param threshold Default 1%, threshold to regroup taxa with lower relative abundance into the 'other' groups
-#' @param n_phy Default 4, number of main_level to plot. Same number of color must be given in the 'hues' parameter. 
+#' @param n_phy Default 4, number of main_level to plot. Same number of color must be given in the 'hues' parameter.
 #' @param hues Color used to represent main_level, should be the same number than n_phy parameter.
 #' @param color_bias Define the gradiant of shades among the colors
 #' @param text_size control the text size. see ggplot2
 #' @param legend_size control the legend text size. see ggplot2
 #' @param x_axis_size control the x axis text size. see ggplot2
 #' @param differential_analysis Default TRUE. Whether or not use DESeq2 to do a differential abundance analysis
-#' @param test Default 'Wald'. see DESeq2 
-#' @param fitType Default "parametric", see DESeq2 
-#' @param sfType Default "ratio",see DESeq2 
-#' @param betaPrior Default FALSE,see DESeq2 
-#' @param reduced Default FALSE,see DESeq2 
-#' @param quiet Default FALSE,see DESeq2 
-#' @param minReplicatesForReplace Default '7',see DESeq2 
-#' @param modelMatrixType Default "standard",see DESeq2 
-#' @param useT Default FALSE,see DESeq2 
-#' @param minmu see DESeq2 
-#' @param parallel Default FALSE,see DESeq2 
-#' @param BPPARAM see DESeq2 
+#' @param test Default 'Wald'. see DESeq2
+#' @param fitType Default "parametric", see DESeq2
+#' @param sfType Default "ratio",see DESeq2
+#' @param betaPrior Default FALSE,see DESeq2
+#' @param reduced Default FALSE,see DESeq2
+#' @param quiet Default FALSE,see DESeq2
+#' @param minReplicatesForReplace Default '7',see DESeq2
+#' @param modelMatrixType Default "standard",see DESeq2
+#' @param useT Default FALSE,see DESeq2
+#' @param minmu see DESeq2
+#' @param parallel Default FALSE,see DESeq2
+#' @param BPPARAM see DESeq2
 #' @param ... additionnal parameters passed into the phyloseq_to_deseq2 functions, see Phyloseq
 #'
 #' @return A list containing  ggplot $plot, object and if differential_analysis = TRUE, a dataframe with DESeq2 outputs of significant features $significant_table
 #'
-#' @examples 
+#' @examples
 #' data(GlobalPatterns)
 #' ps_unfiltered <- GlobalPatterns
-#' my_plot <- plot_gut_microbiota(ps_object = ps_unfiltered, 
-#' exp_group = 'SampleType', 
+#' my_plot <- plot_gut_microbiota(ps_object = ps_unfiltered,
+#' exp_group = 'SampleType',
 #' subset_group = c("Feces", "Skin"),
-#' sample_name = 'X.SampleID', 
+#' sample_name = 'X.SampleID',
 #' differential_analysis = T,
 #' fdr_threshold = 0.8)#'
 #' print(my_plot$plot)
 #' print(my_plot$significant_table)
-#' 
+#'
 #' @export
-#' 
+#'
 #' @author Thibault Cuisiniere, Marco Constante, Manuela M. Santos
-#' 
-#' 
+#'
+#'
 #' @import phyloseq
 #' @import ggplot2
 #' @import ggtext
@@ -58,9 +58,9 @@
 #' @import RColorBrewer
 #' @import forcats
 #' @import DESeq2
-#' 
-#' 
-#' 
+#'
+#'
+#'
 
 
 
@@ -77,7 +77,6 @@ plot_gut_microbiota <- function(ps_object = ps_unfiltered,
                                 text_size = 9,
                                 legend_size = 7,
                                 x_axis_size = 8,
-                                animate = F,
                                 differential_analysis = T,
                                 test = c("Wald", "LRT")[1],
                                 fitType = c("parametric", "local", "mean", "glmGamPoi")[1],
@@ -92,88 +91,89 @@ plot_gut_microbiota <- function(ps_object = ps_unfiltered,
                                 parallel = FALSE,
                                 BPPARAM = bpparam(),
                                 fdr_threshold = 0.05,
+                                sig_lab = T,
                                 ...) {
   #Assure taxa are row
   if (!taxa_are_rows(ps_object)) {
     ps_object <- t(ps_object)
   }
-  
+
   #Subset the samples belonging to the groups selected
   if (is.null(subset_group) == F) {
     keep_samples = as.character(get_variable(ps_object, exp_group)) %in% subset_group
     ps_object = prune_samples(keep_samples, ps_object)
-    
+
   }
-  
+
   #transform in proportional
   ps_prop <-
     transform_sample_counts(ps_object, function(OTU)
       ((OTU / sum(OTU)) * 100))
-  
-  
+
+
   #store TAX, OTU and metadata tables
   otu <-  as.data.frame(otu_table(ps_prop))
   tax <-  as.data.frame(tax_table(ps_prop))
   meta <- data.frame(sample_data(ps_prop))
-  
+
   #clean up OTU absent and taxa not present
-  
+
   otu <- otu %>% filter_all(any_vars(. != 0))
-  
+
   tax <- subset(tax, rownames(tax) %in% rownames(otu))
-  
+
   #Agglomerate at the sub level defined
   ps_f <- tax_glom(ps_prop, taxrank = sub_level , NArm = FALSE)
   #Store OTU and TAX
   otu_f <-  as.data.frame(otu_table(ps_f))
   tax_f <-  as.data.frame(tax_table(ps_f))
-  
+
   #Bind TAX and OTU
-  
+
   otu_tax_f <- cbind(tax_f, otu_f)
-  
-  
+
+
   #create a vector, if True, the ASV mean among the samples is higher than the threshold
-  
+
   ##keep the position of the column storing the abundance of the taxa in the otu_tax_f object
   position <- ncol(tax) + 1:(ncol(otu_tax_f) - ncol(tax))
-  
+
   message(paste0('\n', length(position), ' samples are analyzed \n'))
-  
+
   #Create 2 vectors in the dataframe :
   #1 storing the abundance mean of the taxa
   #2 T or F this taxa is above the define thrshold of filtering
-  
+
   otu_tax_f <- otu_tax_f %>%
     rowwise() %>%
     mutate(Mean = mean(c_across(all_of(position)))) %>%
     mutate(high_abundance = case_when(Mean > threshold  ~ TRUE,
                                       TRUE  ~ FALSE))
-  
+
   #Store top x of the main_level phylogeny
-  
+
   topx <- otu_tax_f %>%
     dplyr::group_by(!!as.name(main_level)) %>%
     dplyr::summarise(sum_top = sum(c_across(position))) %>%
     dplyr::arrange(desc(sum_top)) %>%
     dplyr::slice_head(n = n_phy)
-  
-  
+
+
   #Create a vector, TRUE when the phylum belong to the top X, the ones we want to plot
   otu_tax_f <- mutate(otu_tax_f,
                       selected_top = !!as.name(main_level) %in% pull(topx[, main_level]))
-  
+
   #Create a column to create the legend of the graphs :
   #1 higher than the threshold and in the main_level selected -> name of the sub_level
   #1.1 if sub_level is NA -> Other_main_level_name
-  
+
   #2 belong to the main_level selected, but sub_level bellow the threshold -> other_phylum name
-  
+
   #3 all the others -> "Other"
-  
+
   #replace NA
-  
-  
+
+
   otu_tax_f <- otu_tax_f %>%
     mutate(
       plot_taxa = case_when(
@@ -191,8 +191,8 @@ plot_gut_microbiota <- function(ps_object = ps_unfiltered,
           is.na(!!as.name(sub_level)) == TRUE  ~ paste0("Unknown_",!!as.name(main_level))
       )
     )
-  
-  
+
+
   if (nrow(topx) != length(hues)) {
     message(
       'the number of colors choosen (',
@@ -202,78 +202,78 @@ plot_gut_microbiota <- function(ps_object = ps_unfiltered,
       ')'
     )
   }
-  
-  
+
+
   #initialize the dataframe
   df <-
     as_tibble(matrix(nrow = 0, ncol = length(colnames(otu_tax_f))),
               .name_repair = ~ colnames(otu_tax_f))
-  
+
   #loop through selected phylum to add color
   i <- 1
-  
+
   for (i in 1:nrow(topx)) {
     top_loop <- pull(topx[, main_level])[i]
     # print(top_loop)
-    
+
     # Subset the df with unique features and in function of the Means abundance
-    
+
     temp <- otu_tax_f %>%
       dplyr::filter(!!as.name(main_level) == top_loop) %>%
       dplyr::arrange(desc(Mean))
-    
+
     message(
       'Among the ', main_level, ' ' ,top_loop,' ' ,
       length(unique(temp$plot_taxa)) ,
       ' features at ',sub_level,' level will be plotted'
     )
-    
+
     #define the color palette
     getPalette = colorRampPalette(brewer.pal(length(unique(temp$plot_taxa)), hues[i]), bias = color_bias)
     col <-
       getPalette(length(unique(temp$plot_taxa)) + 1)
-    
-    
+
+
     #loop among the low level feature and add color to it
     u <- 1
     for (u in 1:length(unique(temp$plot_taxa))) {
       print(unique(temp$plot_taxa)[u])
-      
+
       #Add the color
       temp[which(temp$plot_taxa ==  unique(temp$plot_taxa)[u]), 'MyColors'] <-
         col[u + 1]
-      
+
     }
-    
+
     #create the dataframe storing the colors information
     df <- rbind(df, temp)
-    
+
   }
-  
-  
+
+
   #Selected features that don't belong to high abundances level and assigned the black color to them
   unselescted <- otu_tax_f %>%
     filter(!(!!as.name(main_level) %in% pull(topx[, main_level])))
-  
+
   unselescted$MyColors <- '#000000'
-  
+
   #Merge the 2 dataframe
   df <- rbind(df, unselescted)
-  
+
   #order df to define the order that will be plotted, first the selected main_level, then the main_level in alphabetical order, then the mean
-  
+
   df <- df %>% ungroup %>%
     dplyr::arrange(desc(selected_top),!!as.name(main_level) , desc(Mean))
-  
-  
+
+
   #check if no features have been lost
   nrow(df) == nrow(otu_tax_f)
-  
+
   #factor to keep order in the plot
   df$plot_taxa <-
     factor(df$plot_taxa, levels = unique(df$plot_taxa))
-  
-  
+
+
   #Transform df into long format by sample
   df_long <- melt(
     df,
@@ -281,54 +281,73 @@ plot_gut_microbiota <- function(ps_object = ps_unfiltered,
     measure.vars = meta[, sample_name],
     variable.name = sample_name
   )
-  
-  
-  
+
+
+
   #Add exp_group to df_long
   df_long <- left_join(df_long, meta, by = sample_name)
-  
-  
+
+
   # Differential abundance analysis using DESeq2 : 2 groups only
   if (differential_analysis &&
       length(unique(meta[[exp_group]])) == 2) {
     fam_glom <- tax_glom(ps_object, taxrank = sub_level)
-    
+
     #ps_object is already filtered (or not but contains only 2 groups) with user defined group.
-    
+
     #remove OTU with 0 count accros the dataset
-    
+
     if (sum(rowSums(otu_table(ps_object)) == 0) > 0) {
       ps_object <-
         prune_taxa(rowSums(otu_table(ps_object)) > 0, ps_object)
-      
+
     }
-    
+
     # Coerce count data to matrix of integers
     countData = round(as(otu_table(fam_glom), "matrix"), digits = 0)
     colData = data.frame(sample_data(fam_glom))
     # Create the DESeq data set, dds.
-    
+
     diagdds = phyloseq_to_deseq2(fam_glom,  formula(paste("~", exp_group)), ...)
-    
+
     # Run DESeq2 analysis
     diag = DESeq(diagdds, test = test, fitType = fitType)
-    
+
     # Get differentially abundant features
     results <- results(diag)
-    
+
     # Extract features with adjusted p-value below a threshold
     significant_features <- subset(results, padj < fdr_threshold)
-    
+
     #We have significant ASV, we want to link them to the level we are interested (family)
-    
+
     significant_features <- as.data.frame(cbind(significant_features,
                                                 tax_table(fam_glom)[rownames(tax_table(fam_glom)) %in% rownames(significant_features)]))
-    
+
     # Add information to df_long to mark differentially abundant features
     df_long$differential_abundance <- FALSE
     df_long$differential_abundance[df_long$plot_taxa %in% significant_features[, sub_level]] <-
       TRUE
-    
+
+    #Add stars to legends if sig_lab is defined as TRUE
+    significant_features$stars <-""
+
+    if(sig_lab == T) {
+
+      significant_features$stars <- symnum(significant_features$`padj`,
+                                           symbols   = c("***","**","*",""),
+                                           cutpoints = c(0,  .001,.01,.05,1),
+                                           corr      = FALSE
+      )
+
+      #Add stars to the name of the taxa
+      star_vec <- significant_features$stars[match(df_long$plot_taxa , significant_features[, sub_level])]
+      star_vec[is.na(star_vec)]  <-""
+      df_long$plot_taxa <- paste0(df_long$plot_taxa, " ", star_vec )
+
+
+    }
+
     #put in bold significant features
     df_long$legend_label <-
       ifelse(
@@ -339,20 +358,20 @@ plot_gut_microbiota <- function(ps_object = ps_unfiltered,
     df_long$plot_taxa <-   df_long$legend_label
     df_long$plot_taxa <-
       factor(df_long$plot_taxa, levels = unique(df_long$plot_taxa))
-    
+
   }
-  
-  
+
+
   #Prepare the color vector
   MyColors <- df_long$MyColors
   names(MyColors) <- df_long$plot_taxa
   unique(df_long$plot_taxa)
   unique(MyColors)
-  
+
   MyColors2 <- unique(df_long$MyColors)
   names(MyColors2) <- unique(df_long$plot_taxa)
-  
-  
+
+
   p <-
     ggplot(df_long, aes(
       x = !!as.name(sample_name),
@@ -392,19 +411,19 @@ plot_gut_microbiota <- function(ps_object = ps_unfiltered,
     )
   #p<-p+ scale_fill_manual(values=MyColors)
   p <- p + scale_fill_manual('plot_taxa', values = MyColors2)
-  
+
   p <- p + facet_wrap( ~ df_long[[exp_group]] , scales  = "free_x")
-  
+
   p
-  
-  
-  
-  
+
+
+
+
   if (differential_analysis == T) {
     return(list(significant_table = significant_features, plot = p))
-    
+
   } else {
     return(list(plot = p))
   }
-  
+
 }
